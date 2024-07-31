6d1886b39dadb5ae04acf23b412c23e73484256d
Andrew Gonzalez <andrewgonzalezcr7@gmail.com>
2024-07-31 17:46:21 -0400

    Implemented search functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 } 
 
 function App() {
@@ -21,7 +22,7 @@ const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery)
     lg: '200px 1fr', 
   }}>
     <GridItem area='nav' >
-      <NavBar />
+      <NavBar onSearch={(searchText) => setGameQuery({...gameQuery, searchText})} />
     </GridItem>
     <Show above="lg">
       <GridItem area='aside' paddingX={5}>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,7 +20,7 @@ export interface Game{
 
 
 const useGames = (gameQuery: GameQuery) =>{
-    return useData<Game>('/games', {params: {genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder}}, [gameQuery]);
+    return useData<Game>('/games', {params: {genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search: gameQuery.searchText}}, [gameQuery]);
 }
 
 export default useGames;
