77bd9928a49c94f3a831633dae026b2b2e4f14fe
Andrew Gonzalez <andrewgonzalezcr7@gmail.com>
2024-07-31 11:12:28 -0400

    Added a spinner while loading genres and changed the width of the card containers and the side container.

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import useGenres, { Genre } from '../hooks/useGenres'
 import useData from '../hooks/useData';
+import { HStack, Image, List, ListIcon, ListItem, Spinner, Text } from '@chakra-ui/react';
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
-  const {data} = useGenres(); 
+  const {data, isLoading, error} = useGenres(); 
+
+  if(isLoading) return <Spinner />;
+  if(error) return null;
   return (
-    <ul>
-      {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
+    <List>
+      {data.map(genre => <ListItem key={genre.id} paddingY={'5px'}>
+        <HStack>
+          <Image boxSize={'32px'} borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+          <Text fontSize='lg'>
+            {genre.name}
+          </Text>
+        </HStack>
+      </ListItem>)}
+    </List>
   )
 }
 
