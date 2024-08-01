e68392ea26940314096d32dbaf81aae48ab9d6f8
Andrew Gonzalez <andrewgonzalezcr7@gmail.com>
2024-07-31 21:04:08 -0400

    Changed the key to be in an env variable.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '0c0d32d17ed6488a8cbcb2547c262e07'
+        key: import.meta.env.VITE_RAWG_API_KEY,
     }
 })
